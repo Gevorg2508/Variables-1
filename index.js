c79070acf415db5b894615077f4823352884e2b5
Harutyunyan Gevorg <gev.harutyunyan.96@gmail.com>
2021-12-01 15:34:29 +0400

    my js homework

---
+++ index.js
@@ -0,0 +1,27 @@
+// 1.
+// let admin;
+// let name;
+//  name = "John";
+//  admin = name;
+// console.log(admin);
+// 2.
+// let i = 10;
+// let n = i++ % 5;
+ 
+// answers i = 11; n = 0;
+
+// 3.
+// let a = 10,
+// b = 3;
+// let temp;
+// temp = a;
+// a = b;
+// b = temp;
+
+// 4.
+// let a = 10,
+// b = 3;
+// a = a * b;
+// b = a / b;
+// a = a / b;
+
